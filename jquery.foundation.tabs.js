26bb90659a6f0d8c4bd2af0ce07ab698adc114ae
denverboyle258 <79061178+denverboyle258@users.noreply.github.com>
2021-02-15 03:20:27 -0500

    this is first github commit for my student
    hello world commit

---
+++ jquery.foundation.tabs.js
@@ -0,0 +1,34 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  $.fn.foundationTabs = function (options) {
+
+    var settings = $.extend({
+      callback: $.noop
+    }, options);
+
+    var activateTab = function ($tab) {
+      var $activeTab = $tab.closest('dl').find('dd.active'),
+          contentLocation = $tab.children('a').attr("href") + 'Tab';
+
+      contentLocation = contentLocation.replace(/^.+#/, '#');
+
+      $activeTab.removeClass('active');
+      $tab.addClass('active');
+
+      $(contentLocation).closest('.tabs-content').children('li').removeClass('active').hide();
+      $(contentLocation).css('display', 'block').addClass('active');
+    };
+
+    $(document).on('click.fndtn', 'dl.tabs dd a', function (event){
+      activateTab($(this).parent('dd'));
+    });
+
+    if (window.location.hash) {
+      activateTab($('a[href="' + window.location.hash + '"]').parent('dd'));
+      settings.callback();
+    }
+
+  };
+
+})(jQuery, this);
